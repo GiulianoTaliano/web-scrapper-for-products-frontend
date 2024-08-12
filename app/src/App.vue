e93064b1ef325e7f2d67ced24cd3ff85e86307f4
Giuliano Taliano <54330145+GiulianoDolceTalianoSantoro@users.noreply.github.com>
2024-08-12 00:05:45 -0300

    Add integration with API

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,26 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Product Search</h1>
+    <input v-model="productName" placeholder="Enter product name" />
+    <input v-model="countryCode" placeholder="Enter country code" />
+    <button @click="searchProducts">Search</button>
+
+    <div v-if="loading">Loading...</div>
+    
+    <ul v-if="products.length > 0">
+      <li v-for="(product, index) in products" :key="index">
+        <strong>{{ product.name }}</strong> - Relevance Score: {{ product.relevance_score }}
+      </li>
+    </ul>
+
+    <div v-if="error">{{ error }}</div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      productName: '',
+      countryCode: '',
+      products: [],
+      loading: false,
+      error: null
+    };
+  },
+  methods: {
+    async searchProducts() {
+      this.loading = true;
+      this.error = null;
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/api/v1/products', {
+          params: {
+            product_name: this.productName,
+            country_code: this.countryCode
+          }
+        });
+        this.products = response.data;
+      } catch (err) {
+        this.error = 'Failed to fetch products';
+      } finally {
+        this.loading = false;
+      }
+    }
   }
-}
+};
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+}
+
+input {
+  margin: 5px;
 }
 </style>
